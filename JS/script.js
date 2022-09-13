3f8d05c383d7a73140f841734a6bbe348981c84c
Daeron Jones <daeronjones@gmail.com>
2022-09-13 00:25:59 -0400

    fixed responsiveness

--- JS/script.js
+++ JS/script.js
@@ -10,6 +10,16 @@ function openNav() {
     document.getElementById("overNav").style.height = "0%";
   }
 
+  /* Open */
+function openForm() {
+    document.getElementById("contactForm").style.width = "100%";
+  }
+  
+  /* Close */
+  function closeForm() {
+    document.getElementById("contactForm").style.width = "0%";
+  }
+  
 
 
 
@@ -28,3 +38,28 @@ function openNav() {
           } 
       });
   }
+
+
+
+
+
+
+  // Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
